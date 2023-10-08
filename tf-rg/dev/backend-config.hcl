b50f026255f7e621740bccd757b327fec9f24976
Louis Benagha <elitesolutionsit@outlook.com>
2023-10-08 13:43:23 -0500

    poc test for pipeline

---
+++ tf-rg/dev/backend-config.hcl
@@ -0,0 +1,4 @@
+resource_group_name  = "placeholder"
+storage_account_name = "placeholderbuc"
+container_name       = "placeholdercon"
+key                  = "infra-dev.tfstate"
---
+++ tf-rg/tf-provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+  client_id       = "185d67fb-0062-46e8-b73e-ddf81962df93"
+  subscription_id = "f823a9e2-b296-41ab-b452-5f2c01eefae8"
+  client_secret   = "rhI8Q~ZsgjZufUnrPHVEmRbciZumfrZtH7cq3bnB"
+  tenant_id       = "389dc11c-508a-4f1e-a529-1f19aa1a8e7b"
+}
